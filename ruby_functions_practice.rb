950047fa4e180fcc5f2c82daebaa000223cb6946
matapaska <marta.pasikowska@gmail.com>
2019-10-15 16:18:20 +0100

    fixes test of test_number_to_full_name__month_1

--- ruby_functions_practice.rb
+++ ruby_functions_practice.rb
@@ -29,3 +29,32 @@ end
 def add_string_as_number (strin_1, string_2)
   return string_1.to_i() + string_2.to_i()
 end
+
+def test_number_to_full_name__month_1 (num)
+  case num
+    when 1
+      return "January"
+    when 2
+      return "February"
+    when 3
+      return "March"
+    when 4
+      return "April"
+    when 5
+      return "May"
+    when 6
+      return "June"
+    when 7
+      return "July"
+    when 8
+      return "August"
+    when 9
+      return "September"
+    when 10
+      return "October"
+    when 11
+      return "November"
+    when 12
+      return "December"
+    end
+  end
