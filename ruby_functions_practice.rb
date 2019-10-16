3ad820a6377e0a99384020b688f5255975daa3d4
matapaska <marta.pasikowska@gmail.com>
2019-10-16 10:11:15 +0100

    After class review and corrections

--- ruby_functions_practice.rb
+++ ruby_functions_practice.rb
@@ -15,7 +15,7 @@ def test_multiply (num1, num2)
 end
 
 def test_divide(num1, num2)
-  return num1/num2
+  return num1/num2.to_f
 end
 
 def length_of_string (string)
@@ -55,6 +55,54 @@ def number_to_short_month_name (num)
   end
 end
 
+
+# def number_to_full_name_month (num)
+#   case num
+#   when 1
+#     return "January"
+#   when 2
+#     return "February"
+#   when 3
+#     return "March"
+#   when 4
+#     return "April"
+#   when 5
+#     return "May"
+#   when 6
+#     return "June"
+#   when 7
+#     return "July"
+#   when 8
+#     return "August"
+#   when 9
+#     return "September"
+#   when 10
+#     return "October"
+#   when 11
+#     return "November"
+#   when 12
+#     return "December"
+#   end
+# end
+
+#Alternative solution using function from
+#previous excercise using the case and slicing it
+# def number_to_short_month_name (num)
+#   return number_to_full_name_month (num).slice(0,3)
+# end
+
+
 def volume_of_cube(num)
   return num ** 3
+end
+
+def volume_of_sphere(radius)
+  volume = (4.0/3.0) * Math::PI * (radius ** 3)
+  #  or volume = (4.0/3.0) * 3.141592 * (radius ** 3)
+  return volume.round(2)
+end 
+
+def test_fahrenheit_to_celsius(farenheit)
+  celsius = (farenheit - 32) * (5.0/9.0)
+  return celsius.round(2)
 end
